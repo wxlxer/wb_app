41b4c7db8835195507e400f50b86fdd0ea3c70e9
wx <285978810@qq.com>
2019-09-16 09:28:05 +0800

    新增资源

---
+++ app/js/app.js
@@ -0,0 +1,43 @@
+console.log("进来了");
+
+//1、下载配置文件
+//2、检测登录
+//3、进入游戏
+
+
+//登录模块
+//调整游戏模块
+let g_hall = plus.webview.create('hall.html', 'hall', {
+		scrollIndicator: 'none',
+		scalable: false,
+		popGesture: 'close',
+		backButtonAutoControl: 'close'
+		
+	});
+	g_hall.addEventListener('close', function(){
+		console.log("大厅关闭");
+	}, false);
+	
+	console.log("11111111111111111111"); 
+	g_hall.show('zoom-fade-out');
+
+let g_game = null;
+
+function enterGame(args)
+{
+	console.log(args);
+	g_hall.hide();
+	g_game = plus.webview.create("game.html","game");
+	g_game.show('zoom-fade-out');
+	g_game.addEventListener('close', function(){
+		console.log("游戏关闭");
+		g_game = null;
+	}, false);	
+}
+
+function exitGame(args){
+	console.log(args);
+	g_hall.show();
+	g_game.close();
+}
+
